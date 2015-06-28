f332e8c5c2bc1b7d78e1ca636ae97de3a7aa0914
Gray Hound <admin@devilmaydie.name>
2015-06-28 20:54:32 +0300

    Browsers chart.

--- app/controllers/shortener_controller.rb
+++ app/controllers/shortener_controller.rb
@@ -1,4 +1,26 @@
 class ShortenerController < ApplicationController
+  BROWSER_NAMES = {
+      edge: "Microsoft Edge",
+      ie: "Internet Explorer",
+      chrome: "Chrome",
+      firefox: "Firefox",
+      android: "Android",
+      blackberry: "BlackBerry",
+      core_media: "Apple CoreMedia",
+      ipad: "iPad",
+      iphone: "iPhone",
+      ipod: "iPod Touch",
+      nintendo: "Nintendo",
+      opera: "Opera",
+      phantom_js: "PhantomJS",
+      psp: "PlayStation Portable",
+      playstation: "PlayStation",
+      quicktime: "QuickTime",
+      safari: "Safari",
+      xbox: "Xbox",
+      other: "Other",
+  }
+
   def new
     @shortener_url_object = Shortener::ShortenedUrl.new(shortener_shortened_url_params)
     @shortener_url_object.attributes = shortener_shortened_url_params
@@ -40,6 +62,7 @@ class ShortenerController < ApplicationController
     @qr = RQRCode::QRCode.new(url, :size => 4, :level => :h)
 
     @referers_chart = get_referers_chart(@shortened_url)
+    @browsers_chart = get_browsers_chart(@shortened_url)
   end
 
   def shortener_shortened_url_params
@@ -48,8 +71,8 @@ class ShortenerController < ApplicationController
 
   def get_referers_chart(shortened_url)
     referers_table = ::GoogleVisualr::DataTable.new
-    referers_table.new_column("string", "domain")
-    referers_table.new_column("number", "count")
+    referers_table.new_column("string", "Domain")
+    referers_table.new_column("number", "Click count")
 
     data = shortened_url.shortened_url_referers.
                          order(:count => :desc, :updated_at => :desc).
@@ -58,8 +81,25 @@ class ShortenerController < ApplicationController
                          map {|value| [value['domain'], value['count']]}
     referers_table.add_rows(data)
 
-    option = { width: 580, height: 360, title: 'Referers' }
+    option = { width: 580, height: 580, title: 'Referers' }
     return ::GoogleVisualr::Interactive::PieChart.new(referers_table, option)
+  end
+
+  def get_browsers_chart(shortened_url)
+    browsers_table = ::GoogleVisualr::DataTable.new
+    browsers_table.new_column("string", "Browser")
+    browsers_table.new_column("number", "Click count")
+
+    print BROWSER_NAMES[:ie]
+
+    data = shortened_url.shortened_url_browsers.
+        order(:count => :desc, :updated_at => :desc).
+        select(:browser_name, :count).to_a().
+        map {|value| [BROWSER_NAMES[value['browser_name'].to_sym()], value['count']]}
+    browsers_table.add_rows(data)
+
+    option = { width: 580, height: 580, title: 'Browsers' }
+    return ::GoogleVisualr::Interactive::BarChart.new(browsers_table, option)
   end
 
 end
