bcc2e10dca95b632321a5783361907ee6a95d279
Gray Hound <admin@devilmaydie.name>
2015-06-29 19:16:59 +0300

    Since it's just test project at heroku - disable confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :validatable
   has_shortened_urls
 end
